0e866f156d4035910c194427bd47feac2529bb46
LukasGardberg <lukasgardberg2@gmail.com>
2024-02-25 12:15:08 +0100

    more posts

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ type Post struct {
 	Url        string
 	Id         string
 	FaviconUrl string
-	Points 	   int
+	Points     int
 }
 
 type APIResponse struct {
@@ -23,15 +23,20 @@ type APIResponse struct {
 }
 
 var allPosts []Post = make([]Post, 0)
+var shownPosts []Post = make([]Post, 0)
+
+const postsPerPage = 5 
+
+var currentPostIndex = 0
 
 func NewPost(title, url string, points int) Post {
 	// auto generate uuid id
 	return Post{
-		Title: title, 
-		Url: url, 
-		Id: uuid.New().String(), 
+		Title:      title,
+		Url:        url,
+		Id:         uuid.New().String(),
 		FaviconUrl: "https://www.google.com/s2/favicons?domain=" + url,
-		Points: points,
+		Points:     points,
 	}
 }
 
@@ -45,6 +50,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", defaultHandler)
+	r.GET("/more-posts", morePostsHandler)
 	r.POST("/submit-post", submitPostHandler)
 
 	if err := r.Run("127.0.0.1:8000"); err != nil {
@@ -55,13 +61,11 @@ func main() {
 func defaultHandler(c *gin.Context) {
 	tmpl := template.Must(template.ParseFiles("index.html"))
 
-	topPosts := getTopPosts()
-	fmt.Println(topPosts)
-
-	allPosts = topPosts
+	getTopPosts()
+	shownPosts = append(shownPosts, getMorePosts()...)
 
 	posts := map[string][]Post{
-		"Posts": allPosts,
+		"Posts": shownPosts,
 	}
 
 	tmpl.Execute(c.Writer, posts)
@@ -73,12 +77,16 @@ func submitPostHandler(c *gin.Context) {
 
 	post := NewPost(title, url, 0)
 	allPosts = append(allPosts, post)
+	currentPostIndex++
+	fmt.Println("Added post. Current index: ", currentPostIndex)
 
 	tmpl := template.Must(template.ParseFiles("index.html"))
 	tmpl.ExecuteTemplate(c.Writer, "post-element", post)
 }
 
-func getTopPosts() []Post {
+func getTopPosts() {
+	// gets the current top posts and stores them
+
 	url := "http://hn.algolia.com/api/v1/search?tags=front_page"
 	response, err := http.Get(url)
 	if err != nil {
@@ -95,10 +103,37 @@ func getTopPosts() []Post {
 	}
 
 	foundPosts := make([]Post, 0)
-	for i := range apiResponse.Hits {	
+	for i := range apiResponse.Hits {
 		newPost := NewPost(apiResponse.Hits[i].Title, apiResponse.Hits[i].Url, apiResponse.Hits[i].Points)
 		foundPosts = append(foundPosts, newPost)
 	}
 
-	return foundPosts
+	fmt.Println("Adding", len(foundPosts), "posts.")
+	allPosts = append(allPosts, foundPosts...)
+
+}
+
+func getMorePosts() []Post {
+	if currentPostIndex + postsPerPage >= len(allPosts) {
+		// TODO: Fetch more posts
+		fmt.Println("No more posts to show.")
+		return nil
+	}
+
+	postsToShow := allPosts[currentPostIndex : currentPostIndex+postsPerPage]
+	currentPostIndex += postsPerPage
+	return postsToShow
+}
+
+func morePostsHandler(c *gin.Context) {
+	// append more posts onto shown posts and execute
+
+	morePosts := getMorePosts()
+	shownPosts = append(shownPosts, morePosts...)
+
+	tmpl := template.Must(template.ParseFiles("index.html"))
+    
+	for i := range morePosts {
+		tmpl.ExecuteTemplate(c.Writer, "post-element", morePosts[i])
+	}
 }
